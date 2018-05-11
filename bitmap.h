06575e6d7b35c5a544c465c8924cb861036bd5a8
altea09 <altea_09@yahoo.it>
2018-05-11 10:35:15 +0200

    aggiunta stampa bitmap + define per prove

--- bitmap.h
+++ bitmap.h
@@ -1,4 +1,9 @@
 #pragma once
+
+#define BYTE_SIZE 8    //aggiunto
+#define NUM_BITS_BITMAP  16  //aggiunto
+
+
 typedef struct{
   int num_bits;
   char* entries;
@@ -14,7 +19,7 @@ typedef struct {
 BitMapEntryKey BitMap_blockToIndex(int num);
 
 // converts a bit to a linear index
-int BitMap_indexToBlock(int entry, uint8_t bit_num);
+int BitMap_indexToBlock(int entry, char bit_num);
 
 // returns the index of the first bit having status "status"
 // in the bitmap bmap, and starts looking from position start
@@ -22,3 +27,6 @@ int BitMap_get(BitMap* bmap, int start, int status);
 
 // sets the bit at index pos in bmap to status
 int BitMap_set(BitMap* bmap, int pos, int status);
+
+//print the bitmap                                     //aggiunto
+void BitMap_print(BitMap* bmap);
